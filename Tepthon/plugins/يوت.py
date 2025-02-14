c66391d93511c62d2d868b0da9ebe0669b62ec6c
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2025-02-14 22:36:27 +0300

    Update يوت.py

--- Tepthon/plugins/يوت.py
+++ Tepthon/plugins/يوت.py
@@ -3,8 +3,7 @@ import requests
 import yt_dlp
 from telethon import TelegramClient, events
 from youtube_search import YoutubeSearch as B3KKK
-from Tepthon import zedub
-from ..Config import Config
+from telethon import TelegramClient, events # تصحيح الاستيراد
 import glob
 import random
 
